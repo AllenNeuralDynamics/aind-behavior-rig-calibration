8d6f08f3bc8a0af4035be3742860793eba48e5e2
bruno-f-cruz <bfpcruz@gmail.com>
2024-01-13 14:20:21 -0800

    Move file to root

--- regenerate.py
+++ regenerate.py
@@ -4,14 +4,23 @@ from pathlib import Path
 from aind_behavior_rig.base.json_schema import export_schema
 from aind_behavior_rig.olfactometer import OlfactometerCalibrationModel
 from aind_behavior_rig.water_valve import WaterValveCalibrationModel
+from aind_behavior_rig.utils import bonsai_sgen, BonsaiSgenSerializers
 
-OUTPUT_ROOT = Path("./src/DataSchemas/")
+SCHEMA_ROOT = Path("./src/DataSchemas/")
+EXTENSIONS_ROOT = Path("./src/Extensions/")
+
+
+
+a = BonsaiSgenSerializers.JSON
+print(a & BonsaiSgenSerializers.YAML != BonsaiSgenSerializers.NONE)
+print(a)
 
 # Water Valve Calibration
-with open(OUTPUT_ROOT / "water_valve_calibration.json", "w") as f:
+with open(SCHEMA_ROOT / "water_valve_calibration.json", "w") as f:
     f.write(json.dumps(export_schema(WaterValveCalibrationModel), indent=2))
 
 
+
 # Olfactometer Calibration
-with open(OUTPUT_ROOT / "olfactometer_calibration.json", "w") as f:
+with open(SCHEMA_ROOT / "olfactometer_calibration.json", "w") as f:
     f.write(json.dumps(export_schema(OlfactometerCalibrationModel), indent=2))
