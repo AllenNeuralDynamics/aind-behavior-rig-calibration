45b964b0b3c9cb52c1dc6443ee6cd3a20ec4ac93
bruno-f-cruz <bfpcruz@gmail.com>
2024-01-15 13:57:47 -0800

    Add olfactometer calibration example

---
+++ examples/olfactometer.py
@@ -0,0 +1,57 @@
+from aind_behavior_rig.olfactometer import (
+    HarpOlfactometerChannel,
+    OlfactometerCalibrationModel,
+    OlfactometerChannel,
+    OlfactometerChannelConfig,
+    OlfactometerOperationControl,
+    RigCalibrationFullModel,
+)
+
+from aind_data_schema.models.devices import ChannelType
+
+channels_config = [
+    OlfactometerChannel(
+        channel_index=HarpOlfactometerChannel.Channel0,
+        channel_type=ChannelType.ODOR,
+        flow_capacity=100),
+    OlfactometerChannel(
+        channel_index=HarpOlfactometerChannel.Channel1,
+        channel_type=ChannelType.ODOR,
+        flow_capacity=100),
+    OlfactometerChannel(
+        channel_index=HarpOlfactometerChannel.Channel2,
+        channel_type=ChannelType.ODOR,
+        flow_capacity=100),
+    OlfactometerChannel(
+        channel_index=HarpOlfactometerChannel.Channel3,
+        channel_type=ChannelType.CARRIER,
+        flow_capacity=1000)
+]
+channels_config = {channel.channel_index: channel for channel in channels_config}
+
+stimuli_config = [
+    OlfactometerChannelConfig(
+        channel_index=HarpOlfactometerChannel.Channel0,
+        odorant="Banana",
+        odorant_dilution=.1),
+    OlfactometerChannelConfig(
+        channel_index=HarpOlfactometerChannel.Channel1,
+        odorant="Banana",
+        odorant_dilution=.1),
+    OlfactometerChannelConfig(
+        channel_index=HarpOlfactometerChannel.Channel2,
+        odorant="Banana",
+        odorant_dilution=.1)
+]
+stimuli_config = {channel.channel_index: channel for channel in stimuli_config}
+
+
+OpControl = OlfactometerOperationControl(
+    channel_config=channels_config,
+    stimulus_config=stimuli_config,
+    full_flow_rate=1000,
+    n_repeats_per_stimulus=10,
+    time_on=1,
+    time_off=1)
+
+print(OpControl.model_dump_json(indent=2))
