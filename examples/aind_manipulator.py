b114bb634deabb4980879a09842e581444fcf21e
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-04-05 08:47:35 -0700

    Fix examples

--- examples/aind_manipulator.py
+++ examples/aind_manipulator.py
@@ -1,9 +1,11 @@
 from aind_behavior_services.calibration import aind_manipulator as m
 from aind_behavior_services.base import get_commit_hash
+from aind_behavior_services.session import AindBehaviorSessionModel
 import datetime
 
 
-calibration = m.AindManipulatorCalibration(
+calibration_data = m.AindManipulatorCalibration(
+    name="AindManipulatorCalibration",
     input=m.AindManipulatorCalibrationInput(
         full_step_to_mm=m.ManipulatorPosition(x=0.01, y1=0.01, y2=0.01, z=0.01),
         axis_configuration=[
@@ -19,17 +21,22 @@ calibration = m.AindManipulatorCalibration(
     calibration_date=datetime.datetime.now(),
 )
 
+calibration_logic = m.AindManipulatorCalibrationLogic()
 
-out_model = m.AindManipulatorCalibrationModel(
-    calibration=calibration,
+
+calibration_session = AindBehaviorSessionModel(
     root_path="C:\\Data",
+    remote_path=None,
     allow_dirty_repo=False,
-    experiment="AindManipulatorSettings",
+    experiment="AindManipulatorCalibration",
+    date=datetime.datetime.now(),
     subject="AindManipulator",
     experiment_version="manipulator_control",
-    commit_hash=get_commit_hash()
+    commit_hash=get_commit_hash(),
 )
 
-
-with open("local/aind_manipulator.json", "w") as f:
-    f.write(out_model.model_dump_json(indent=3))
+seed_path = "local/aind_manipulator_{suffix}.json"
+with open(seed_path.format(suffix="calibration_logic"), "w") as f:
+    f.write(calibration_logic.model_dump_json(indent=3))
+with open(seed_path.format(suffix="session"), "w") as f:
+    f.write(calibration_session.model_dump_json(indent=3))
--- examples/olfactometer.py
+++ examples/olfactometer.py
@@ -1,61 +1,69 @@
-from aind_behavior_services.base import get_commit_hash
 import datetime
 
+from aind_behavior_services.session import AindBehaviorSessionModel
+from aind_behavior_services.base import get_commit_hash
+
 from aind_behavior_services.calibration.olfactometer import (
-    HarpOlfactometerChannel,
+    OlfactometerChannelType,
     OlfactometerChannel,
     OlfactometerChannelConfig,
-    OlfactometerOperationControl,
-    OlfactometerCalibrationModel,
-    ChannelType
+    OlfactometerCalibrationLogic,
+    OlfactometerCalibration, OlfactometerCalibrationInput, OlfactometerCalibrationOutput
 )
 
 
 channels_config = [
-    OlfactometerChannel(
-        channel_index=HarpOlfactometerChannel.Channel0, channel_type=ChannelType.ODOR, flow_capacity=100
+    OlfactometerChannelConfig(
+        channel_index=OlfactometerChannel.Channel0,
+        channel_type=OlfactometerChannelType.ODOR,
+        flow_rate=100,
+        odorant="Banana",
+        odorant_dilution=0.1,
     ),
-    OlfactometerChannel(
-        channel_index=HarpOlfactometerChannel.Channel1, channel_type=ChannelType.ODOR, flow_capacity=100
+    OlfactometerChannelConfig(
+        channel_index=OlfactometerChannel.Channel1,
+        channel_type=OlfactometerChannelType.ODOR,
+        flow_rate=100,
+        odorant="Strawberry",
+        odorant_dilution=0.1,
     ),
-    OlfactometerChannel(
-        channel_index=HarpOlfactometerChannel.Channel2, channel_type=ChannelType.ODOR, flow_capacity=100
+    OlfactometerChannelConfig(
+        channel_index=OlfactometerChannel.Channel2,
+        channel_type=OlfactometerChannelType.ODOR,
+        flow_rate=100,
+        odorant="Apple",
+        odorant_dilution=0.1,
     ),
-    OlfactometerChannel(
-        channel_index=HarpOlfactometerChannel.Channel3, channel_type=ChannelType.CARRIER, flow_capacity=1000
+    OlfactometerChannelConfig(
+        channel_index=OlfactometerChannel.Channel3, channel_type=OlfactometerChannelType.CARRIER, odorant="Air"
     ),
 ]
+
 channels_config = {channel.channel_index: channel for channel in channels_config}
 
-stimuli_config = [
-    OlfactometerChannelConfig(channel_index=HarpOlfactometerChannel.Channel0, odorant="Banana", odorant_dilution=0.1),
-    OlfactometerChannelConfig(channel_index=HarpOlfactometerChannel.Channel1, odorant="Banana", odorant_dilution=0.1),
-    OlfactometerChannelConfig(channel_index=HarpOlfactometerChannel.Channel2, odorant="Banana", odorant_dilution=0.1),
-]
+calibration = OlfactometerCalibration(input=OlfactometerCalibrationInput(), output=OlfactometerCalibrationOutput())
 
-stimuli_config = {channel.channel_index: channel for channel in stimuli_config}
-
-
-OpControl = OlfactometerOperationControl(
+calibration_logic = OlfactometerCalibrationLogic(
     channel_config=channels_config,
-    stimulus_config=stimuli_config,
     full_flow_rate=1000,
     n_repeats_per_stimulus=10,
     time_on=1,
     time_off=1,
 )
 
-out_model = OlfactometerCalibrationModel(
-    operation_control=OpControl,
-    calibration=None,
+calibration_session = AindBehaviorSessionModel(
     root_path="C:\\Data",
+    remote_path=None,
     date=datetime.datetime.now(),
     allow_dirty_repo=False,
     experiment="OlfactometerCalibration",
     experiment_version="0.0.0",
     subject="Olfactometer",
-    commit_hash=get_commit_hash()
+    commit_hash=get_commit_hash(),
 )
 
-with open("local/olfactometer.json", "w") as f:
-    f.write(out_model.model_dump_json(indent=3))
+seed_path = "local/olfactometer_{suffix}.json"
+with open(seed_path.format(suffix="calibration_logic"), "w") as f:
+    f.write(calibration_logic.model_dump_json(indent=3))
+with open(seed_path.format(suffix="session"), "w") as f:
+    f.write(calibration_session.model_dump_json(indent=3))
