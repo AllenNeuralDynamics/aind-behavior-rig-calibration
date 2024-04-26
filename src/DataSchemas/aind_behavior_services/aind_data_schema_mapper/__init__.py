0fd9f93a59d564e4f61b8fd1c356241fc7c304f1
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-04-26 08:29:12 -0700

    Handle optional dependency

---
+++ src/DataSchemas/aind_behavior_services/aind_data_schema_mapper/__init__.py
@@ -0,0 +1,7 @@
+try:
+    import aind_data_schema
+except ImportError as e:
+    e.add_note(
+        "The 'aind-data-schema' package is required to use this module. Install the optional dependencies defined in `project.toml' by running `pip install .[aind-data-schema-mapper]`"
+    )
+    raise
