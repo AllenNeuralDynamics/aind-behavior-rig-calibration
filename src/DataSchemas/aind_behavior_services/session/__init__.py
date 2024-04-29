37b975d2c676f852bc2e5be398ee4f7c9f56a30d
brunocruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-04-29 10:44:47 -0700

    Revert "Add experimenter to schema"

--- src/DataSchemas/aind_behavior_services/session/__init__.py
+++ src/DataSchemas/aind_behavior_services/session/__init__.py
@@ -1,6 +1,6 @@
 # Import core types
 from datetime import datetime
-from typing import Literal, Optional, List
+from typing import Literal, Optional
 
 from aind_behavior_services.base import SchemaVersionedModel
 
@@ -13,7 +13,6 @@ __version__ = "0.1.1"
 class AindBehaviorSessionModel(SchemaVersionedModel):
     schema_version: Literal[__version__] = __version__
     experiment: str = Field(..., description="Name of the experiment")
-    experimenter: List[str] = Field(default=[], description="Name of the experimenter")
     date: datetime = Field(default_factory=datetime.now, description="Date of the experiment")
     root_path: str = Field(..., description="Root path where data will be logged")
     remote_path: Optional[str] = Field(
