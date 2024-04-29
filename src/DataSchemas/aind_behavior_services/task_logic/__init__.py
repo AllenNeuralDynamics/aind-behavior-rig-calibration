801b11de45e42b18c661de252273562170c44dc2
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-04-29 15:01:09 -0700

    Add name of task/stage to task logic schema

--- src/DataSchemas/aind_behavior_services/task_logic/__init__.py
+++ src/DataSchemas/aind_behavior_services/task_logic/__init__.py
@@ -6,6 +6,7 @@ from pydantic import Field, field_validator
 
 class AindBehaviorTaskLogicModel(SchemaVersionedModel):
     rng_seed: Optional[float] = Field(default=None, description="Seed of the random number generator")
+    name: Optional[str] = Field(default=None, description="Optional name of the task or stage")
 
     @field_validator("schema_version", mode="before")
     @classmethod
