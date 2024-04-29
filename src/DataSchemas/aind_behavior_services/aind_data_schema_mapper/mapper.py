8c2e5adb7bc837732b4e204f6896939f2a6925a3
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-04-29 10:40:54 -0700

    Move files

--- src/DataSchemas/aind_behavior_services/aind_data_schema_mapper/mapper.py
+++ src/DataSchemas/aind_behavior_services/aind_data_schema_mapper/mapper.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from typing import Type, TypeVar, Union
 
-from aind_data_schema.core.session import Session, Stream, Modality
+from aind_data_schema.core.session import Session, Stream, Modality, StimulusEpoch, Software
 from aind_behavior_services import AindBehaviorSessionModel, AindBehaviorRigModel, AindBehaviorTaskLogicModel
 
 
@@ -17,12 +17,10 @@ task_logic_path = session_root / "tasklogic_input.json"
 rig_path = session_root / "rig_input.json"
 
 
-
-
 def map_to_aind_data_schema(
-        session: TSession,
-        rig: TRig,
-        task_logic: TTaskLogic,
+    session: TSession,
+    rig: TRig,
+    task_logic: TTaskLogic,
 ) -> Session:
 
     ads_session = Session(
@@ -31,6 +29,7 @@ def map_to_aind_data_schema(
         session_type=session.experiment,
         rig_id=rig.rig_name,
         subject_id=session.subject,
+        notes=session.notes,
         data_streams=[
             Stream(
                 stream_modalities=[Modality.BEHAVIOR, Modality.BEHAVIOR_VIDEOS],
@@ -41,6 +40,26 @@ def map_to_aind_data_schema(
         ],
         mouse_platform_name="Mouse platform",
         active_mouse_platform=True,
+        stimulus_epochs=StimulusEpoch(
+            stimulus_start_time=session.date,
+            stimulus_end_time=session.date,
+            stimulus_modalities=[Modality.BEHAVIOR],
+            software=Software(
+                name="Bonsai",
+                version=session.commit_hash,
+                url="https://github.com/AllenNeuralDynamics/Aind.Behavior.VrForaging/blob/{sha}/bonsai/Bonsai.config".format(
+                    sha=session.commit_hash
+                ),
+            ),
+            script=Software(
+                name="vr-foraging.bonsai",
+                version=session.commit_hash,
+                url="https://github.com/AllenNeuralDynamics/Aind.Behavior.VrForaging/blob/{sha}/src/vr-foraging.bonsai".format(
+                    sha=session.commit_hash
+                ),
+                parameters=task_logic,
+            ),
+        ),
     )
     return ads_session
 
@@ -48,5 +67,3 @@ def map_to_aind_data_schema(
 def model_from_json_file(json_path: os.PathLike | str, model_class: Type[TSchema]) -> TSchema:
     with open(json_path, encoding="utf-8") as f:
         return model_class.model_validate_json(f.read())
-
-
