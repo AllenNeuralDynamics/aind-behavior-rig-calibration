2287248be847023dc62a62e7acabd756de446af7
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-04-29 10:59:53 -0700

    Add experimenter to schema

--- src/DataSchemas/aind_behavior_services/session/__init__.py
+++ src/DataSchemas/aind_behavior_services/session/__init__.py
@@ -1,18 +1,19 @@
 # Import core types
 from datetime import datetime
-from typing import Literal, Optional
+from typing import Literal, Optional, List
 
 from aind_behavior_services.base import SchemaVersionedModel
 
 # Import aind-datas-schema types
 from pydantic import Field
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 
 class AindBehaviorSessionModel(SchemaVersionedModel):
     schema_version: Literal[__version__] = __version__
     experiment: str = Field(..., description="Name of the experiment")
+    experimenter: List[str] = Field(default=[], description="Name of the experimenter")
     date: datetime = Field(default_factory=datetime.now, description="Date of the experiment")
     root_path: str = Field(..., description="Root path where data will be logged")
     remote_path: Optional[str] = Field(
