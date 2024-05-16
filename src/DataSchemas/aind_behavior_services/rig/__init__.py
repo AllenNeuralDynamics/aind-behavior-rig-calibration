f30092f9f0b4b0fc4c7a75dfdb3cf0e44fde44b8
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-05-16 10:42:40 -0700

    Add display color adjustment settings to Screen device

--- src/DataSchemas/aind_behavior_services/rig/__init__.py
+++ src/DataSchemas/aind_behavior_services/rig/__init__.py
@@ -174,6 +174,8 @@ class Screen(Device):
     target_update_frequency: float = Field(default=120, description="Target update frequency")
     calibration_directory: str = Field(default="Calibration\\Monitors\\", description="Calibration directory")
     texture_assets_directory: str = Field(default="Textures", description="Calibration directory")
+    brightness: float = Field(default=1, le=1, ge=0, description="Brightness")
+    contrast: float = Field(default=1, le=1, ge=0, description="Contrast")
 
 
 class Treadmill(BaseModel):
