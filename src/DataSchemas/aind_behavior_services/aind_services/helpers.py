8b133c2566c2caa1f8834e50f14274ef2bebc68a
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-08-04 15:08:07 -0700

    Modify method signature to return keys of found objects

--- src/DataSchemas/aind_behavior_services/aind_services/helpers.py
+++ src/DataSchemas/aind_behavior_services/aind_services/helpers.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, Type, Any, Iterable, Dict, List, Union, get_args
+from typing import TypeVar, Type, Iterable, Dict, List, Union, get_args, Tuple, Optional
 import pydantic
 
 from aind_behavior_services.rig import CameraController, CameraTypes, AindBehaviorRigModel
@@ -10,7 +10,7 @@ def get_cameras(
     rig_instance: AindBehaviorRigModel, exclude_without_video_writer: bool = True
 ) -> Dict[str, CameraTypes]:
     cameras: dict[str, CameraTypes] = {}
-    camera_controllers = get_fields_of_type(rig_instance, CameraController)
+    camera_controllers = [x[1] for x in get_fields_of_type(rig_instance, CameraController)]
 
     for controller in camera_controllers:
         if exclude_without_video_writer:
@@ -26,41 +26,38 @@ _ISearchableTypeChecker = tuple(get_args(ISearchable))  # pre-compute for perfor
 
 
 def get_fields_of_type(
-    searchable: ISearchable, target_type: Type[T], /, is_recursive: bool = True, stop_recursion_on_type: bool = True
-) -> List[T]:
-    """_summary_
-
-    Args:
-        searchable (ISearchable): A searchable object.
-        target_type (Type[T]): Target type to search for.
-        is_recursive (bool, optional): Whether to perform a recursive search. Defaults to True.
-        stop_recursion_on_type (bool, optional): Whether to stop recursive search when a target type is found. Defaults to True.
-
-    Raises:
-        ValueError: Unsupported model type.
-
-    Returns:
-        List[T]: A list with all objects of the target type
-    """
-    result: list[T] = []
-
-    _iterable: Iterable[Any]
+    searchable: ISearchable,
+    target_type: Type[T],
+    *args,
+    is_recursive: bool = True,
+    stop_recursion_on_type: bool = True,
+    **kwargs,
+) -> List[Tuple[Optional[str], T]]:
+    _iterable: Iterable
     _is_type: bool
+    result: List[Tuple[Optional[str], T]] = []
 
     if isinstance(searchable, dict):
-        _iterable = list(searchable.values())
+        _iterable = searchable.items()
     elif isinstance(searchable, list):
-        _iterable = searchable
+        _iterable = list(zip([None for _ in range(len(searchable))], searchable))
     elif isinstance(searchable, pydantic.BaseModel):
-        _iterable = [getattr(searchable, field) for field in searchable.model_fields.keys()]
+        _iterable = {k: getattr(searchable, k) for k in searchable.model_fields.keys()}.items()
     else:
         raise ValueError(f"Unsupported model type: {type(searchable)}")
 
-    for field in _iterable:
+    for name, field in _iterable:
         _is_type = False
         if isinstance(field, target_type):
-            result.append(field)
+            result.append((name, field))
             _is_type = True
         if is_recursive and isinstance(field, _ISearchableTypeChecker) and not (stop_recursion_on_type and _is_type):
-            result.extend(get_fields_of_type(field, target_type, is_recursive))
+            result.extend(
+                get_fields_of_type(
+                    field,
+                    target_type,
+                    is_recursive=is_recursive,
+                    stop_recursion_on_type=stop_recursion_on_type,
+                )
+            )
     return result
