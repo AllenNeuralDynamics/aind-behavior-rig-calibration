8466f68a4ffd3ea0bf6c753e47aac32f8840af40
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-08-08 17:43:09 -0700

    Add Trigger delays

--- scripts/setup_watchdog.ps1
+++ scripts/setup_watchdog.ps1
@@ -1,13 +1,15 @@
 #$url = "https://github.com/AllenNeuralDynamics/aind-watchdog-service/actions/runs/9781777954/artifacts/1664898054"
-
-#$outputPath = Join-Path -Path $PSScriptRoot -ChildPath "watchdog.exe"
-$outputPath = (Join-Path -Path "./scripts" -ChildPath "watchdog.exe") 
+$outputPath = Join-Path -Path $PSScriptRoot -ChildPath "watchdog.exe"
 $outputPath = $outputPath | Resolve-Path
 #Invoke-WebRequest -Uri $url -OutFile $outputPath
 
+
 $taskAction = New-ScheduledTaskAction -Execute "$outputPath" # TODO add default arguments via cli if/when PR gets merged
-$taskTriggerStartup = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:00:30
-$taskTriggerLogOn = New-ScheduledTaskTrigger -AtLogOn -RandomDelay 00:00:30
+$taskTriggerStartup = New-ScheduledTaskTrigger -AtStartup
+$taskTriggerLogOn = New-ScheduledTaskTrigger -AtLogOn
+$taskTriggerStartup.Delay = "PT30S"
+$taskTriggerLogOn.Delay = "PT30S"
+
 $taskSettings = New-ScheduledTaskSettingsSet -DontStopOnIdleEnd -ExecutionTimeLimit '00:00:00'
 $taskPrincipal = New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType S4U -RunLevel Highest
 $taskPath = "AIND"
