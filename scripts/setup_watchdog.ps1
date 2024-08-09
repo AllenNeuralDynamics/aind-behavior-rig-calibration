df005fc1298a128e99c70fa509b1d7c6860eba67
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-08-08 17:34:41 -0700

    Add draft of watchdog deployment script

---
+++ scripts/setup_watchdog.ps1
@@ -0,0 +1,19 @@
+#$url = "https://github.com/AllenNeuralDynamics/aind-watchdog-service/actions/runs/9781777954/artifacts/1664898054"
+
+#$outputPath = Join-Path -Path $PSScriptRoot -ChildPath "watchdog.exe"
+$outputPath = (Join-Path -Path "./scripts" -ChildPath "watchdog.exe") 
+$outputPath = $outputPath | Resolve-Path
+#Invoke-WebRequest -Uri $url -OutFile $outputPath
+
+$taskAction = New-ScheduledTaskAction -Execute "$outputPath" # TODO add default arguments via cli if/when PR gets merged
+$taskTriggerStartup = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:00:30
+$taskTriggerLogOn = New-ScheduledTaskTrigger -AtLogOn -RandomDelay 00:00:30
+$taskSettings = New-ScheduledTaskSettingsSet -DontStopOnIdleEnd -ExecutionTimeLimit '00:00:00'
+$taskPrincipal = New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType S4U -RunLevel Highest
+$taskPath = "AIND"
+$taskName = "aind-watchdog-service"
+if (Get-ScheduledTask -TaskPath ("\" + $taskPath + "\") -TaskName $taskName -ErrorAction SilentlyContinue) {
+    Unregister-ScheduledTask -TaskPath ("\" + $taskPath + "\") -TaskName $taskName -Confirm:$false
+}
+$fullTaskPath = "\" + $taskPath + "\" + $taskName
+Register-ScheduledTask -TaskName $fullTaskPath -Action $taskAction -Trigger @($taskTriggerStartup, $taskTriggerLogOn) -Settings $taskSettings -Principal $taskPrincipal
