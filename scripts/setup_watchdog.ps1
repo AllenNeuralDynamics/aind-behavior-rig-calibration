da547ae88b2b4eac1f36c95de4b38e24be22b430
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-09-06 14:33:06 -0700

    Refactor watchdog deployment

--- scripts/setup_watchdog.ps1
+++ scripts/setup_watchdog.ps1
@@ -9,19 +9,21 @@ foreach ($path in @($outputPath, $manifestsPath, $completedPath)) {
     }
 }
 
+$c = Get-Credential -UserName $env:USERNAME -Message "Enter user credentials:"
 $exePath = Join-Path -Path $outputPath -ChildPath "watchdog.exe"
 $response = Invoke-WebRequest -Uri $url -OutFile $exePath -TimeoutSec 5
-$taskAction = New-ScheduledTaskAction -Execute "$exePath -f $manifestsPath -m $completedPath"
+$taskAction = New-ScheduledTaskAction -Execute "$exePath" -Argument "-f $manifestsPath -m $completedPath"
 $taskTriggerStartup = New-ScheduledTaskTrigger -AtStartup
 $taskTriggerLogOn = New-ScheduledTaskTrigger -AtLogOn
 $taskTriggerStartup.Delay = "PT30S"
 $taskTriggerLogOn.Delay = "PT30S"
-$taskSettings = New-ScheduledTaskSettingsSet -DontStopOnIdleEnd -ExecutionTimeLimit '00:00:00'
-$taskPrincipal = New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType S4U -RunLevel Highest
+$taskSettings = New-ScheduledTaskSettingsSet -DontStopOnIdleEnd -ExecutionTimeLimit '00:00:00' -Hidden
+$taskPrincipal = New-ScheduledTaskPrincipal -UserId $c.UserName -LogonType Password -RunLevel Highest
 $taskPath = "AIND"
 $taskName = "aind-watchdog-service"
 if (Get-ScheduledTask -TaskPath ("\" + $taskPath + "\") -TaskName $taskName -ErrorAction SilentlyContinue) {
     Unregister-ScheduledTask -TaskPath ("\" + $taskPath + "\") -TaskName $taskName -Confirm:$false
 }
 $fullTaskPath = "\" + $taskPath + "\" + $taskName
-Register-ScheduledTask -TaskName $fullTaskPath -Action $taskAction -Trigger @($taskTriggerStartup, $taskTriggerLogOn) -Settings $taskSettings -Principal $taskPrincipal
+Register-ScheduledTask -TaskName $fullTaskPath -Action $taskAction -Trigger @($taskTriggerStartup, $taskTriggerLogOn) -Settings $taskSettings -Principal $taskPrincipal -Password $c.Password -User $c.UserName
+
